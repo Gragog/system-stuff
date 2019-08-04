4fa60a9c9abf04cca3e821c1235e61d970b350a3
Gramini <4258666-Gramini@users.noreply.gitlab.com>
2019-08-04 10:00:37 +0200

    removed bashrc_ubuntu; stuff is put in ~/system-stuff

--- install-skripts/install-files.php
+++ install-skripts/install-files.php
@@ -8,19 +8,24 @@ if ($argc !== 2) {
 $root = __DIR__ . '/../';
 $user = get_current_user();
 $home = '/home/' . $user . '/';
+$ssRoot = $home . 'system-stuff/';
+
+if (!is_dir($ssRoot) && !mkdir($ssRoot) && !is_dir($ssRoot)) {
+    throw new \RuntimeException(sprintf('Directory "%s" was not created', $ssRoot));
+}
 
 if ($argv[1] === 'test') {
-    $mapping = [$root . 'test-file-pls-ignore'   => $home . 'test-file-pls-ignore'];
+    $mapping = [$root . 'test-file-pls-ignore'   => $ssRoot . 'test-file-pls-ignore'];
 }
 
 if ($argv[1] === 'yes') {
     $mapping = [
-        $root . '.bash_aliases'   => $home . '.bash_aliases',
-        $root . '.bashrc_general' => $home . '.bashrc_general',
-        $root . '.bashrc_ubuntu'  => $home . '.bashrc_ubuntu',
-        $root . '.ps1'            => $home . '.ps1',
-        $root . '.zshrc'          => $home . '.zshrc',
-        $root . 'dash-to-panel.ini' => $home . 'dash-to-panel.ini',
+        $root . '.bash_aliases'     => $ssRoot . '.bash_aliases',
+        $root . '.bashrc_general'   => $ssRoot . '.bashrc_general',
+        $root . '.bashrc_ubuntu'    => $ssRoot . '.bashrc_ubuntu',
+        $root . '.ps1'              => $ssRoot . '.ps1',
+        $root . '.zshrc'            => $ssRoot . '.zshrc',
+        $root . 'dash-to-panel.ini' => $ssRoot . 'dash-to-panel.ini',
     ];
 }
 
@@ -29,29 +34,12 @@ if (!isset($mapping)) {
     exit(1);
 }
 
-$backupPath = $home . '.system-stuff-backup-before-install/';
-
-if (!is_dir($backupPath) && !mkdir($backupPath, 0755, true) && !is_dir($backupPath)) {
-    throw new RuntimeException(sprintf('Directory "%s" was not created', $backupPath));
-}
-
 foreach ($mapping as $from => $to) {
-    // Backup
-    $fileBackupPath = $backupPath . basename($from);
-    if (file_exists($to)) {
-        if (!file_exists($fileBackupPath)) {
-            echo 'Backing up ' . $to . '';
-            copy($to, $fileBackupPath);
-            echo "\tdone" . PHP_EOL;
-        }
-        unlink($to);
-    }
-
     // Install
     echo 'Installing ' . $to . '';
     symlink($from, $to);
     echo "\tdone" . PHP_EOL;
 }
 
-echo PHP_EOL . 'You still have to make sure that your ".bashrc" sources ".bashrc_ubuntu"' . PHP_EOL;
+echo PHP_EOL . 'You still have to make sure that your ".bashrc" sources "~/system-stuff/.bashrc_general"' . PHP_EOL;
 echo PHP_EOL;
